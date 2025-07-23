87016119603bb13025b703add5530bbe15078469
Kris Hemenway <krishemenway@gmail.com>
2025-07-23 01:13:29 -0700

    removed extra type definition

--- ItemList/ItemSortTypes/SortByName.ts
+++ ItemList/ItemSortTypes/SortByName.ts
@@ -1,9 +1,8 @@
-import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
 import { SortByString } from "Common/Sort";
 import { ItemSortOption } from "ItemList/ItemSortOption";
 
 export const SortByName: ItemSortOption = {
 	labelKey: "LabelName",
 	field: "Name",
-	sortFunc: SortByString<BaseItemDto>((i) => i.Name),
+	sortFunc: SortByString((i) => i.Name),
 };
