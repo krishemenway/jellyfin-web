4e685ce019af3904f16b402cac668e71bfa207ce
Kris Hemenway <krishemenway@gmail.com>
2025-07-27 00:15:41 -0700

    using SortName for sorting by name

--- ItemList/ItemSortTypes/SortByName.ts
+++ ItemList/ItemSortTypes/SortByName.ts
@@ -4,5 +4,5 @@ import { ItemSortOption } from "ItemList/ItemSortOption";
 export const SortByName: ItemSortOption = {
 	labelKey: "LabelName",
 	field: "Name",
-	sortFunc: SortByString((i) => i.Name),
+	sortFunc: SortByString((i) => i.SortName),
 };
