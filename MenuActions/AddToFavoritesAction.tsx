959472e0fbc16f8f11fbf8cd96bdfda6ec634dfc
Kris Hemenway <krishemenway@gmail.com>
2025-08-05 18:10:48 -0700

    updated add to favorites action to apply to all provided items to accommodate bulk actions later

--- MenuActions/AddToFavoritesAction.tsx
+++ MenuActions/AddToFavoritesAction.tsx
@@ -3,13 +3,11 @@ import { ItemMenuAction } from "Items/ItemMenuAction";
 import { ItemFavoriteIcon } from "Items/ItemFavoriteIcon";
 import { getUserLibraryApi } from "@jellyfin/sdk/lib/utils/api/user-library-api";
 import { ServerService } from "Servers/ServerService";
-import { Linq } from "Common/MissingJavascriptFunctions";
 
 export const AddToFavoritesAction: ItemMenuAction = {
 	icon: (p) => <ItemFavoriteIcon {...p} />,
 	textKey: "AddToFavorites",
 	action: (items) => {
-		const item = Linq.Single(items, () => true);
-		getUserLibraryApi(ServerService.Instance.CurrentApi).markFavoriteItem({ itemId: item.Id ?? "", userId: ServerService.Instance.CurrentUserId });
+		Promise.all(items.map((item) => getUserLibraryApi(ServerService.Instance.CurrentApi).markFavoriteItem({ itemId: item.Id ?? "", userId: ServerService.Instance.CurrentUserId })));
 	},
 };
