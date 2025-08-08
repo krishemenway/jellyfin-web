07fd6152ca3fa9603d9aa6e69248c9629cdc9e1b
Kris Hemenway <krishemenway@gmail.com>
2025-08-07 19:03:29 -0700

    adding server name to page titles

--- Common/PageTitle.tsx
+++ Common/PageTitle.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import { TranslationRequest, useTranslatedText } from "Common/TranslatedText";
 import { Nullable } from "Common/MissingJavascriptFunctions";
+import { ServerService } from "Servers/ServerService";
 
 export const PageTitle: React.FC<{ text: string|TranslationRequest|undefined|null }> = (props) => {
 	if (!Nullable.HasValue(props.text)) {
 		return <></>;
 	}
 
-	const text = typeof props.text === "string" ? props.text : useTranslatedText(props.text.Key, props.text.KeyProps);
-	React.useEffect(() => { document.title = text ?? ""; }, [text]);
+	const text = typeof props.text === "string" ? props.text : useTranslatedText(props.text.Key, props.text.KeyProps) ?? "";
+	React.useEffect(() => { document.title = ServerService.Instance.CurrentServer.Name + " | " + text; }, [text, ServerService.Instance.CurrentServer.Name]);
 
 	return <></>;
 };
