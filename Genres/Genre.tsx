8d1cd4ed80b46ad1357d79c5eb1bdae2bfdef81e
Kris Hemenway <krishemenway@gmail.com>
2025-08-15 18:42:12 -0700

    created view for list of genres and each genre page that lists each related item. these needs lots of improvement

--- Genres/Genre.tsx
+++ Genres/Genre.tsx
@@ -1,14 +1,57 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
 import { PageWithNavigation } from "NavigationBar/PageWithNavigation";
+import { PageTitle } from "Common/PageTitle";
+import { Loading } from "Common/Loading";
+import { ResponsiveBreakpoint, useBreakpoint } from "AppStyles";
+import { Nullable } from "Common/MissingJavascriptFunctions";
+import { NotFound } from "Common/NotFound";
+import { ItemService } from "Items/ItemsService";
+import { LoadingIcon } from "Common/LoadingIcon";
+import { LoadingErrorMessages } from "Common/LoadingErrorMessages";
+import { Layout } from "Common/Layout";
+import { ListOf } from "Common/ListOf";
+import { ItemsGridItem } from "ItemList/ItemGridItem";
+import { ImageShape } from "Items/ItemImage";
 
 export const Genre: React.FC = () => {
-	const routeParams = useParams<{ genre: string }>();
-	// This should include content from all libraries available on server to user.
+	const breakpoint = useBreakpoint();
+	const genre = useParams().genre;
+
+	if (!Nullable.HasValue(genre)) {
+		return <PageWithNavigation icon="Genre"><NotFound /></PageWithNavigation>;
+	}
+
+	const studioData = ItemService.Instance.FindOrCreateItemData(genre);
+
+	React.useEffect(() => studioData.LoadChildrenWithAbort(false, { recursive: true, genres: [ genre ] }), [genre]);
+	
 
 	return (
 		<PageWithNavigation icon="Genre">
-			Genre {routeParams.genre}
+			<Layout direction="column" gap="1em" py="1em" height="100%">
+				<PageTitle text={genre} />
+				<Loading
+					receivers={[studioData.Children]}
+					whenNotStarted={<LoadingIcon alignSelf="center" size="4em" />}
+					whenLoading={<LoadingIcon alignSelf="center" size="4em" />}
+					whenError={(errors) => <LoadingErrorMessages errorTextKeys={errors} />}
+					whenReceived={(items) => (
+						<ListOf
+							items={items}
+							direction="row" wrap gap=".5em"
+							forEachItem={(item, index) => (
+								<ItemsGridItem
+									key={item.Id ?? index.toString()}
+									item={item}
+									itemsPerRow={breakpoint === ResponsiveBreakpoint.Desktop ? 9 : breakpoint === ResponsiveBreakpoint.Tablet ? 6 : 2}
+									shape={ImageShape.Portrait}
+								/>
+							)}
+						/>
+					)}
+				/>
+			</Layout>
 		</PageWithNavigation>
 	)
 };
--- Genres/Genres.tsx
+++ Genres/Genres.tsx
@@ -1,12 +1,51 @@
 import * as React from "react";
 import { PageWithNavigation } from "NavigationBar/PageWithNavigation";
+import { Loading } from "Common/Loading";
+import { ItemFilterService } from "Items/ItemFilterService";
+import { LoadingIcon } from "Common/LoadingIcon";
+import { LoadingErrorMessages } from "Common/LoadingErrorMessages";
+import { UserViewStore } from "Users/UserViewStore";
+import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
+import { ListOf } from "Common/ListOf";
+import { HyperLink } from "Common/HyperLink";
+import { PageTitle } from "Common/PageTitle";
+import { Layout } from "Common/Layout";
 
 export const Genres: React.FC = () => {
-	// This should include all the genres available on all servers to the user.
-
 	return (
 		<PageWithNavigation icon="Genre">
-			All Genres
+			<Layout direction="column" gap="2em" py="1em">
+				<PageTitle text={{ Key: "Genres" }} />
+				<Loading
+					receivers={[UserViewStore.Instance.UserViews]}
+					whenNotStarted={<LoadingIcon alignSelf="center" size="4em" />}
+					whenLoading={<LoadingIcon alignSelf="center" size="4em" />}
+					whenError={(errors) => <LoadingErrorMessages errorTextKeys={errors} />}
+					whenReceived={(libraries) => <GenresForLibraries libraries={libraries} />}
+				/>
+			</Layout>
 		</PageWithNavigation>
-	)
+	);
+};
+
+const GenresForLibraries: React.FC<{ libraries: BaseItemDto[] }> = (props) => {
+	const receiver = ItemFilterService.Instance.FindOrCreateFiltersReceiver(props.libraries.map((l) => l.Id ?? ""));
+
+	React.useEffect(() => ItemFilterService.Instance.LoadFiltersWithAbort(props.libraries.map((l) => l.Id ?? "")), [props.libraries])
+
+	return (
+		<Loading
+			receivers={[receiver]}
+			whenNotStarted={<LoadingIcon alignSelf="center" size="4em" />}
+			whenLoading={<LoadingIcon alignSelf="center" size="4em" />}
+			whenError={(errors) => <LoadingErrorMessages errorTextKeys={errors} />}
+			whenReceived={(filters) => (
+				<ListOf
+					direction="row" wrap gap=".5em" py="1em"
+					items={filters.Genres ?? []}
+					forEachItem={(item) => <HyperLink key={item} to={`/Genres/${item}`} direction="row" px=".25em" py=".25em" children={item} />}
+				/>
+			)}
+		/>
+	);
 };
