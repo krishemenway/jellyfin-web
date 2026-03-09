da787bc5734d4758c971c62b0a6b38d635d372e6
Kris Hemenway <krishemenway@gmail.com>
2026-03-08 18:57:13 -0700

    removed class from grid item image

--- ItemList/ItemGridItem.tsx
+++ ItemList/ItemGridItem.tsx
@@ -1,27 +1,26 @@
 import * as React from "react";
 import { BaseItemDto, ImageType } from "@jellyfin/sdk/lib/generated-client/models";
-import { createStyles, useBackgroundStyles } from "AppStyles";
+import { useBackgroundStyles } from "AppStyles";
 import { ImageShape, ItemImage } from "Items/ItemImage";
 import { LinkToItem } from "Items/LinkToItem";
 import { Layout } from "Common/Layout";
 
 export const ItemsGridItem: React.FC<{ item: BaseItemDto; library: BaseItemDto, imageType?: ImageType; shape: ImageShape; itemsPerRow: number }> = (props) => {
 	const background = useBackgroundStyles();
-	const classes = useItemGridClasses();
 	const width = props.shape !== ImageShape.Landscape ? 220 : 330;
 	const height = props.shape !== ImageShape.Portrait ? 220 : 330;
 
 	return (
-		<LinkToItem item={props.item} className={`${background.button}`} direction="column" justifyContent="space-between" alignItems="center" py=".5em" px=".5em" gap="1em" width={{ itemsPerRow: props.itemsPerRow, gap: ".5em" }}>
-			<ItemImage item={props.item} fallback={props.library} className={classes.itemImage} type={props.imageType ?? ImageType.Primary} fillWidth={width} fillHeight={height} lazy objectFit="cover" grow />
+		<LinkToItem
+			item={props.item}
+			className={`${background.button}`}
+			direction="column"
+			py=".5em" px=".5em" gap="1em"
+			justifyContent="space-between" alignItems="center"
+			width={{ itemsPerRow: props.itemsPerRow, gap: ".5em" }}
+		>
+			<ItemImage item={props.item} fallback={props.library} type={props.imageType ?? ImageType.Primary} fillWidth={width} fillHeight={height} lazy objectFit="cover" maxWidth="100%" grow />
 			<Layout direction="column" py=".25em" textAlign="center">{props.item.Name}</Layout>
 		</LinkToItem>
 	);
 };
-
-const useItemGridClasses = createStyles({
-	itemImage: {
-		maxWidth: "100%",
-		objectFit: "contain",
-	},
-});
