f3f59aa21f4c2cf9bc292a6af7e4313690be6b91
Kris Hemenway <krishemenway@gmail.com>
2026-03-10 00:18:18 -0700

    fixed item rating to deal with empty string for CustomRating

--- Items/ItemRating.tsx
+++ Items/ItemRating.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
 import { Layout, StyleLayoutProps } from "Common/Layout";
 import { useBackgroundStyles } from "AppStyles";
+import { Linq, Nullable } from "Common/MissingJavascriptFunctions";
 
 export const ItemRating: React.FC<{ item: BaseItemDto }&StyleLayoutProps> = (props) => {
 	const background = useBackgroundStyles();
 	const combined = {...{fontSize: "1.5em", px: ".25em", py: ".25em" } as StyleLayoutProps, ...props as StyleLayoutProps};
-	const rating = props.item.CustomRating ?? props.item.OfficialRating;
+	const rating = Linq.Coalesce([props.item.CustomRating, props.item.OfficialRating], "", (r) => Nullable.StringHasValue(r));
 
 	return rating && <Layout direction="row" {...combined} className={background.alternatePanel} >{rating}</Layout>;
 };
