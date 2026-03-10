ba996c696318b5b0959e19322efc56c243a4c76b
Kris Hemenway <krishemenway@gmail.com>
2026-03-10 00:45:39 -0700

    added less than/greater than support for official rating filter and added more ratings to mapping in sort by official rating

--- ItemList/ItemFilterTypes/FilterByOfficialRating.tsx
+++ ItemList/ItemFilterTypes/FilterByOfficialRating.tsx
@@ -4,19 +4,96 @@ import { Layout } from "Common/Layout";
 import { AutoCompleteFieldEditor } from "Common/SelectFieldEditor";
 import { EmptyOperation, NotEmptyOperation } from "ItemList/FilterOperations/EmptyOperation";
 import { EqualOperation } from "ItemList/FilterOperations/EqualOperation";
+import { FilterOperation } from "ItemList/FilterOperation";
+import { Nullable } from "Common/MissingJavascriptFunctions";
+import { GetOrder } from "ItemList/ItemSortTypes/SortByOfficialRating";
+import { SortByNumber, SortByObjects } from "Common/Sort";
+
+export const LessThanRatingOperation: FilterOperation = {
+	Name: "LessThan",
+	Display: (filterValue) => typeof filterValue === "string" ? ["LessThanFilterDisplay", filterValue] : [],
+	Operation: (value, filterValue) => {
+		if (!Nullable.HasValue(value)
+			|| !Nullable.HasValue(filterValue)
+			|| typeof value === "number"
+			|| typeof value === "boolean"
+			|| Array.isArray(value)
+			|| Array.isArray(filterValue)) {
+			return false;
+		}
+
+		return GetOrder(value) < GetOrder(filterValue);
+	},
+};
+
+export const LessThanOrEqualsRatingOperation: FilterOperation = {
+	Name: "LessThanOrEquals",
+	Display: (filterValue) => typeof filterValue === "string" ? ["LessThanOrEqualsFilterDisplay", filterValue] : [],
+	Operation: (value, filterValue) => {
+		if (!Nullable.HasValue(value)
+			|| !Nullable.HasValue(filterValue)
+			|| typeof value === "number"
+			|| typeof value === "boolean"
+			|| Array.isArray(value)
+			|| Array.isArray(filterValue)) {
+			return false;
+		}
+
+		return GetOrder(value) <= GetOrder(filterValue);
+	},
+};
+
+export const GreaterThanRatingOperation: FilterOperation = {
+	Name: "GreaterThan",
+	Display: (filterValue) => typeof filterValue === "string" ? ["GreaterThanFilterDisplay", filterValue] : [],
+	Operation: (value, filterValue) => {
+		if (!Nullable.HasValue(value)
+			|| !Nullable.HasValue(filterValue)
+			|| typeof value === "number"
+			|| typeof value === "boolean"
+			|| Array.isArray(value)
+			|| Array.isArray(filterValue)) {
+			return false;
+		}
+
+		return GetOrder(value) > GetOrder(filterValue);
+	},
+};
+
+export const GreaterThanOrEqualsRatingOperation: FilterOperation = {
+	Name: "GreaterThanOrEquals",
+	Display: (filterValue) => typeof filterValue === "string" ? ["GreaterThanOrEqualsFilterDisplay", filterValue] : [],
+	Operation: (value, filterValue) => {
+		if (!Nullable.HasValue(value)
+			|| !Nullable.HasValue(filterValue)
+			|| typeof value === "number"
+			|| typeof value === "boolean"
+			|| Array.isArray(value)
+			|| Array.isArray(filterValue)) {
+			return false;
+		}
+
+		return GetOrder(value) >= GetOrder(filterValue);
+	},
+};
+
 
 export const FilterByOfficialRating: ItemFilterType = {
 	type: "FilterByOfficialRating",
 	labelKey: "LabelParentalRating",
 	targetField: (item) => item.OfficialRating,
 	editor: (props) => {
+		const orderedRatings = React.useMemo(() => SortByObjects(props.filters.OfficialRatings ?? [], [
+			{ LabelKey: "", Reversed: false, SortType: "SortOrder", Sort: SortByNumber(rating => GetOrder(rating)) },
+		]), [props.filters.OfficialRatings]);
+
 		if (props.currentOperation === EmptyOperation || props.currentOperation === NotEmptyOperation) {
 			return <></>;
 		}
 
 		return (
 			<Layout direction="column">
-				<AutoCompleteFieldEditor field={props.filter.FilterValue} allOptions={props.filters.OfficialRatings ?? []} getValue={(rating) => rating} getLabel={(rating) => rating} />
+				<AutoCompleteFieldEditor field={props.filter.FilterValue} allOptions={orderedRatings} getValue={(rating) => rating} getLabel={(rating) => rating} />
 			</Layout>
 		);
 	},
@@ -24,5 +101,9 @@ export const FilterByOfficialRating: ItemFilterType = {
 		EqualOperation,
 		EmptyOperation,
 		NotEmptyOperation,
+		LessThanRatingOperation,
+		LessThanOrEqualsRatingOperation,
+		GreaterThanRatingOperation,
+		GreaterThanOrEqualsRatingOperation,
 	],
 };
--- ItemList/ItemSortTypes/SortByOfficialRating.ts
+++ ItemList/ItemSortTypes/SortByOfficialRating.ts
@@ -1,27 +1,86 @@
+import { Nullable } from "Common/MissingJavascriptFunctions";
 import { SortByNumber } from "Common/Sort";
 import { ItemSortOption } from "ItemList/ItemSortOption";
 
 export const SortByOfficialRating: ItemSortOption = {
 	labelKey: 'OptionParentalRating',
 	field: "OfficialRating",
-	sortFunc: SortByNumber((i) => RatingSortOrder[i.OfficialRating ?? ""] ?? 999),
+	sortFunc: SortByNumber((i) => GetOrder(i.OfficialRating)),
 };
 
-const RatingSortOrder: Record<string, number|undefined> = {
+export function GetOrder(value: string|undefined|null): number {
+	if (!Nullable.HasValue(value)) {
+		return MaxRatingOrder;
+	}
+
+	return RatingSortOrder[value] ?? MaxRatingOrder;
+}
+
+export const MaxRatingOrder = 999;
+export const RatingSortOrder: Record<string, number|undefined> = {
 	"G": 0,
 	"TV-G": 0,
+	"TP": 0,
 	"TV-Y": 25,
 	"TV-Y7": 40,
+	"TV-Y7-FV": 40,
 	"PG": 50,
 	"TV-PG": 50,
+	"TV-PG-D": 50,
+	"TV-PG-L": 50,
+	"TV-PG-S": 50,
+	"TV-PG-V": 50,
+	"TV-PG-DL": 50,
+	"TV-PG-DS": 50,
+	"TV-PG-DV": 50,
+	"TV-PG-LS": 50,
+	"TV-PG-LV": 50,
+	"TV-PG-SV": 50,
+	"TV-PG-DLS": 50,
+	"TV-PG-DLV": 50,
+	"TV-PG-DSV": 50,
+	"TV-PG-LSV": 50,
+	"TV-PG-DLSV": 50,
 	"PG-13": 60,
+	"13+": 60,
 	"TV-14": 60,
+	"TV-14-D": 60,
+	"TV-14-L": 60,
+	"TV-14-S": 60,
+	"TV-14-V": 60,
+	"TV-14-DL": 60,
+	"TV-14-DS": 60,
+	"TV-14-DV": 60,
+	"TV-14-LS": 60,
+	"TV-14-LV": 60,
+	"TV-14-SV": 60,
+	"TV-14-DLS": 60,
+	"TV-14-DLV": 60,
+	"TV-14-DSV": 60,
+	"TV-14-LSV": 60,
+	"TV-14-DLSV": 60,
 	"14": 60,
 	"15": 70,
+	"MA15+": 73,
+	"16+": 73,
+	"18+": 75,
 	"R": 75,
 	"TV-MA": 75,
+	"TV-MA-L": 75,
+	"TV-MA-S": 75,
+	"TV-MA-V": 75,
+	"TV-MA-LS": 75,
+	"TV-MA-LV": 75,
+	"TV-MA-SV": 75,
+	"TV-MA-LSV": 75,
 	"M": 75,
 	"NC-17": 100,
 	"Not Rated": 200,
+	"Unrated": 200,
 	"NR": 200,
+	"TV-X": 200,
+	"TV-AO": 200,
+	"21": 200,
+	"XXX": 200,
+	"Banned": 300,
 };
