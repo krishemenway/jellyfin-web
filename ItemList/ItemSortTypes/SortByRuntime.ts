364bc619b84b089f75d71ba836f4621cb61a4339
Kris Hemenway <krishemenway@gmail.com>
2026-05-10 22:56:57 -0700

    updated content for sort by runtime to show duration string instead of ticks

--- ItemList/ItemSortTypes/SortByRuntime.ts
+++ ItemList/ItemSortTypes/SortByRuntime.ts
@@ -1,9 +1,10 @@
+import { DateTime } from "Common/MissingJavascriptFunctions";
 import { SortByNumber } from "Common/Sort";
 import { ItemSortOption } from "ItemList/ItemSortOption";
 
 export const SortByRuntime: ItemSortOption = {
 	labelKey: 'Runtime',
 	field: "Runtime",
-	getContent: (i) => i.RunTimeTicks?.toString(),
+	getContent: (i) => DateTime.ConvertTicksToDurationString(i.RunTimeTicks),
 	sortFunc: SortByNumber((i) => i.RunTimeTicks),
 };
