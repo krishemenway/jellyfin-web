68f5ff91f05b8cd6092e9b93ee96e2f23d6090a3
Kris Hemenway <krishemenway@gmail.com>
2026-06-05 21:47:22 -0700

    changed item rating to use parental ratings from item metadata

--- Items/ItemRating.tsx
+++ Items/ItemRating.tsx
@@ -3,41 +3,35 @@ import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
 import { Layout, StyleLayoutProps } from "Common/Layout";
 import { useBackgroundStyles } from "AppStyles";
 import { Linq, Nullable } from "Common/MissingJavascriptFunctions";
-import { SelectFieldEditor } from "Common/SelectFieldEditor";
+import { AutoCompleteFieldEditor } from "Common/SelectFieldEditor";
 import { EditableItemProps } from "Items/EditableItemProps";
 import { LoadingIcon } from "Common/LoadingIcon";
-import { ItemFilterService } from "Items/ItemFilterService";
+import { ItemEditorService } from "Items/ItemEditorService";
 import { Loading } from "Common/Loading";
 
 export const ItemRating: React.FC<{ item: BaseItemDto; libraryId: string; }&EditableItemProps&StyleLayoutProps> = (props) => {
 	const background = useBackgroundStyles();
-	const combined = {...{fontSizeREM: 1.5, px: ".25em", py: ".25em" } as StyleLayoutProps, ...props as StyleLayoutProps};
+	const combined = {...{px: ".25em", py: ".25em" } as StyleLayoutProps, ...props as StyleLayoutProps};
 	const rating = Linq.Coalesce([props.item.CustomRating, props.item.OfficialRating], "", (r) => Nullable.StringHasValue(r));
 
-	React.useEffect(() => ItemFilterService.Instance.LoadFiltersWithAbort([props.libraryId]), [props.libraryId]);
-
-	if (props.isEditing && Nullable.HasValue(props.libraryId)) {
+	if (props.isEditing && Nullable.HasValue(props.editableItem) && Nullable.HasValue(props.libraryId)) {
 		return (
-			<Layout direction="row" {...combined} className={background.alternatePanel}>
+			<Layout direction="row" {...combined} minWidth="10rem" className={background.alternatePanel}>
 				<Loading
-					receivers={[ItemFilterService.Instance.FindOrCreateFiltersReceiver([props.libraryId])]}
-					whenError={() => <></>}
-					whenLoading={<LoadingIcon alignSelf="center" size="1em" />}
-					whenNotStarted={<LoadingIcon alignSelf="center" size="1em" />}
-					whenReceived={(filters) => 
-						Nullable.HasValue(props.editableItem?.OfficialRating) ? (
-							<SelectFieldEditor
-								allOptions={filters.OfficialRatings ?? []}
-								field={props.editableItem.OfficialRating}
-								getLabel={(v) => v}
-								getValue={(v) => v ?? ""}
-							/>
-						) : <></>
+					receivers={[ItemEditorService.Instance.MetadataInfo]} whenError={() => <></>}
+					whenLoading={<LoadingIcon alignSelf="center" size="1em" />} whenNotStarted={<LoadingIcon alignSelf="center" size="1em" />}
+					whenReceived={(metadata) =>
+						<AutoCompleteFieldEditor
+							allOptions={metadata.ParentalRatingOptions?.map((r) => r.Name ?? "") ?? []}
+							field={props.editableItem!.OfficialRating}
+							getLabel={(v) => v}
+							getValue={(v) => v ?? ""}
+						/>
 					}
 				/>
 			</Layout>
 		);
 	}
 
-	return Nullable.StringValue(rating, <></>, (r) => <Layout direction="row" {...combined} className={background.alternatePanel} children={r} />);
+	return Nullable.StringValue(rating, <></>, (r) => <Layout direction="row" fontSizeREM={1.5} {...combined} className={background.alternatePanel} children={r} />);
 };
