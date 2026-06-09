79c6fce1f7c3cd26ed918f5d5c6f3e0a6611942f
Kris Hemenway <krishemenway@gmail.com>
2026-06-08 23:44:23 -0700

    resetting item cache when item is marked as favorite from action;

--- MenuActions/AddToFavoritesAction.tsx
+++ MenuActions/AddToFavoritesAction.tsx
@@ -3,11 +3,14 @@ import { ItemMenuAction } from "Items/ItemMenuAction";
 import { ItemFavoriteIcon } from "Items/ItemFavoriteIcon";
 import { getUserLibraryApi } from "@jellyfin/sdk/lib/utils/api/user-library-api";
 import { ServerService } from "Servers/ServerService";
+import { ItemCacheResetService } from "Items/ItemCacheResetService";
 
 export const AddToFavoritesAction: ItemMenuAction = {
 	icon: (p) => <ItemFavoriteIcon {...p} />,
 	textKey: "AddToFavorites",
 	action: (items) => {
-		Promise.all(items.map((item) => getUserLibraryApi(ServerService.Instance.CurrentApi).markFavoriteItem({ itemId: item.Id ?? "", userId: ServerService.Instance.CurrentUserId.Value })));
+		Promise.all(items.map((item) => getUserLibraryApi(ServerService.Instance.CurrentApi).markFavoriteItem({ itemId: item.Id ?? "", userId: ServerService.Instance.CurrentUserId.Value }).then(() => {
+			ItemCacheResetService.Instance.ResetItem(item);
+		})));
 	},
 };
