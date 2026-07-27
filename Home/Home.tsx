ad04b734e973b9367731d27a9eeb150c9948b163
Kris Hemenway <krishemenway@gmail.com>
2026-07-26 19:47:03 -0700

    fixed readonly item list view options labels not showing up when loading

--- Home/Home.tsx
+++ Home/Home.tsx
@@ -124,7 +124,7 @@ const LoadingSection: React.FC<{ label: string; itemsPerRow: number; viewOptions
 	return (
 		<Layout direction="column" gap=".25rem" width="100%">
 			<Layout direction="row" justifyContent="space-between" fontSizeREM={1.3}>
-				<HyperLink to={listUrl} direction="row" px=".25rem" py=".25rem" alignItems="end" children={label} />
+				<HyperLink to={listUrl} direction="row" px=".25rem" py=".25rem" alignItems="end" label={viewOptions.DefaultLabel} children={label} />
 			</Layout>
 
 			<ListOf
