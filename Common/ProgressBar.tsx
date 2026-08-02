9c3930baa506afe9967391cc3fc6eb89558cdfd5
Kris Hemenway <krishemenway@gmail.com>
2026-08-02 14:56:28 -0700

    fixed issue with progress bar background color being broken since disallowing direct color code setting

--- Common/ProgressBar.tsx
+++ Common/ProgressBar.tsx
@@ -1,15 +1,11 @@
 import * as React from "react";
 import { Layout } from "Common/Layout";
-import { useObservable } from "@residualeffect/rereactor";
-import { ThemeService } from "Themes/ThemeService";
 import { Property } from "csstype";
 
 export const ProgressBar: React.FC<{ height?: Property.Height, percentage: number }> = (props) => {
-	const theme = useObservable(ThemeService.Instance.CurrentTheme);
-
 	return (
 		<Layout direction="row" width="100%">
-			<Layout direction="row" width={`${props.percentage}%`} height={props.height} backgroundColor={theme.PrimaryTextColor} />
+			<svg width={`${props.percentage}%`} height={props.height}><rect width="100%" height="100%" /></svg>
 		</Layout>
 	);
 };
@@ -29,6 +25,6 @@ export const ProgressCircle: React.FC<{ size: string; percentage: number }> = ({
 		</svg>
 	);
 };
+
 const progressCircleRadius = 70;
 const progressCircleWindowSize = 160;
-
