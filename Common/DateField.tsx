c32880a841b1ee22938a24bfbe8c24bafdf7c19a
Kris Hemenway <krishemenway@gmail.com>
2026-08-10 21:36:55 -0700

    updated DateField to automatically go to next date part when max length is met; added date with time of day support editor component that uses the same formatting strategy;

--- Common/DateField.tsx
+++ Common/DateField.tsx
@@ -7,55 +7,103 @@ import { TranslationService } from "Common/TranslatedText";
 import { Nullable } from "Common/MissingJavascriptFunctions";
 
 interface DateFieldProps extends StyleLayoutProps {
-	field: EditableField<string|undefined|null> | EditableField<string> | EditableField<string|undefined>;
+	field: EditableField<string|undefined|null> | EditableField<string> | EditableField<string|null> | EditableField<string|undefined>;
 	disabled?: boolean;
 	classes?: string[];
 }
 
-export const DateField: React.FC<DateFieldProps> = ({ field, disabled, ...props }) => {
-	const [datePartRefsByIndex, setDatePartsRefs] = React.useState<Record<number, HTMLInputElement|null>>({});
+const maxLengthsByPart: Record<string, number> = { "year": 4, "month": 2, "day": 2, "hour": 2, "minute": 2, "second": 2 };
+const dateToFormat = new Date();
+
+function dateToParts(date: string): Record<string, string> {
+	const [dayParts, timeOfDayParts] = date.split("T");
+	const [year, month, day] = (dayParts ?? "").split("-");
+	const [hour, minute, second] = (timeOfDayParts ?? "").split(":");
+	return { "year": year, "month": month, "day": day, "hour": hour, "minute": minute, "second": second };
+}
+
+const Empty: Record<string, string> = { "year": "", "month": "", "day": "", "hour": "", "minute": "", "second": ""  }
+
+const dateOptions: Intl.DateTimeFormatOptions = { year: "numeric", month: "numeric", day: "numeric" };
+export const DateField: React.FC<DateFieldProps> = ({ ...props }) => {
+	const currentValue = Nullable.Value(useObservable(props.field.Current), Empty, (cv) => dateToParts(cv));
 	const culture = useObservable(TranslationService.Instance.CurrentCulture);
-	const dateParts = Intl.DateTimeFormat(culture).formatToParts(new Date());
-	const currentDateParts = useObservable(field.Current)?.split("-") ?? [];
+	const dateParts = React.useMemo(() => Intl.DateTimeFormat(culture, dateOptions).formatToParts(dateToFormat), [culture]);
+
+	return <DateFieldForParts dateParts={dateParts} current={currentValue} {...props} />
+}
+
+const dateTimeOptions: Intl.DateTimeFormatOptions = { year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric", };
+export const DateTimeField: React.FC<DateFieldProps> = ({ ...props }) => {
+	const currentValue = Nullable.Value(useObservable(props.field.Current), Empty, (cv) => dateToParts(cv));
+	const culture = useObservable(TranslationService.Instance.CurrentCulture);
+	const dateParts = React.useMemo(() => Intl.DateTimeFormat(culture, dateTimeOptions).formatToParts(dateToFormat), [culture]);
+
+	return <DateFieldForParts dateParts={dateParts} current={currentValue} {...props} />
+}
+
+const DateFieldForParts: React.FC<DateFieldProps&{ dateParts: Intl.DateTimeFormatPart[]; current: Record<string, string> }> = ({ field, disabled, dateParts, current, ...props }) => {
+	const [datePartRefsByIndex, setDatePartsRefs] = React.useState<Record<number, HTMLInputElement|null>>({});
 	const hasError = useComputed(() => !field.CanMakeRequest());
+	const withTimeParts = React.useMemo(() => dateParts.filter(dp => dp.type === "hour").length === 1, [dateParts]);
 	const addRefFunc = (ref: HTMLInputElement|null, index: number) => {
 		datePartRefsByIndex[index] = ref;
 		setDatePartsRefs(datePartRefsByIndex);
 	};
+
+	const gotoNextPart = React.useCallback((afterIndex: number) => {
+		const nextParts = dateParts.slice(afterIndex + 1);
+		const nextPart = nextParts.first((p) => p.type !== "literal");
+
+		if (nextPart === undefined) {
+			return false;
+		}
+
+		const nextPartIndex = dateParts.indexOf(nextPart);
+		const element = datePartRefsByIndex[nextPartIndex];
+
+		if (Nullable.HasValue(element)) {
+			window.setTimeout(() => { element.focus(); }, 100);
+			return true;
+		}
+	}, [dateParts, datePartRefsByIndex]);
+
+	const gotoNextPartWhenReady = React.useCallback((newValue: string, part: string, index: number) => {
+		if (newValue.length >= maxLengthsByPart[part]) {
+			gotoNextPart(index);
+		}
+	}, [gotoNextPart]);
 
 	const onKeyDown = React.useCallback((index: number, key: string) => {
 		const nextParts = dateParts.slice(index + 1);
 		const nextLiteral = nextParts.first((p) => p.type === "literal");
 
 		if (Nullable.HasValue(nextLiteral) && key === nextLiteral.value) {
-			const nextPart = nextParts.first((p) => p.type !== "literal");
-
-			if (nextPart === undefined) {
-				return true;
-			}
-
-			const nextPartIndex = dateParts.indexOf(nextPart);
-			const element = datePartRefsByIndex[nextPartIndex];
-
-			if (Nullable.HasValue(element)) {
-				element.focus();
-				return false;
-			}
+			gotoNextPart(index);
+			return false;
 		}
 
 		return true;
-	}, [dateParts, datePartRefsByIndex])
+	}, [dateParts]);
 
 	return (
 		<Layout direction="row" justifyContent="center" alignItems="center" backgroundColor={hasError ? "Error" : "Field"} {...props}>
 			{dateParts.map((part, index) => {
+				const id = `${field.FieldId}-${part.type}`;
+
 				switch (part.type) {
+					case "second":
+						return <Input key={id} id={id} part={part} disabled={disabled} currentValue={current} width="2em" minLength={2} setRefFunc={r => addRefFunc(r, index)} onChange={newValue => { gotoNextPartWhenReady(newValue, part.type, index); field.OnChange(`${current["year"]}-${current["month"]}-${current["day"]}T${current["hour"]}:${current["minute"]}:${newValue}`); }} onKeyDown={k => onKeyDown(index, k)} />;
+					case "minute":
+						return <Input key={id} id={id} part={part} disabled={disabled} currentValue={current} width="2em" minLength={2} setRefFunc={r => addRefFunc(r, index)} onChange={newValue => { gotoNextPartWhenReady(newValue, part.type, index); field.OnChange(`${current["year"]}-${current["month"]}-${current["day"]}T${current["hour"]}:${newValue}:${current["second"]}`); }} onKeyDown={k => onKeyDown(index, k)} />;
+					case "hour":
+						return <Input key={id} id={id} part={part} disabled={disabled} currentValue={current} width="2em" minLength={2} setRefFunc={r => addRefFunc(r, index)} onChange={newValue => { gotoNextPartWhenReady(newValue, part.type, index); field.OnChange(`${current["year"]}-${current["month"]}-${current["day"]}T${newValue}:${current["minute"]}:${current["second"]}`); }} onKeyDown={k => onKeyDown(index, k)} />;
 					case "day":
-						return <Input key={field.FieldId+"-"+part.type} id={field.FieldId+"-"+part.type} currentValue={currentDateParts[2] ?? ""} part={part.type} width="2em" disabled={disabled} setRefFunc={(r) => addRefFunc(r, index)} onChange={(newValue) => field.OnChange(`${currentDateParts[0] ?? ""}-${currentDateParts[1] ?? ""}-${newValue}`)} classes={["edit-date-day"]} minLength={2} onKeyDown={(k) => onKeyDown(index, k)} />;
+						return <Input key={id} id={id} part={part} disabled={disabled} currentValue={current} width="2em" minLength={2} setRefFunc={r => addRefFunc(r, index)} onChange={newValue => { gotoNextPartWhenReady(newValue, part.type, index); field.OnChange(`${current["year"]}-${current["month"]}-${newValue}${withTimeParts ? `T${current["hour"]}:${current["minute"]}:${current["second"]}` : ""}`); }} onKeyDown={k => onKeyDown(index, k)} />;
 					case "month":
-						return <Input key={field.FieldId+"-"+part.type} id={field.FieldId+"-"+part.type} currentValue={currentDateParts[1] ?? ""} part={part.type} width="2em" disabled={disabled} setRefFunc={(r) => addRefFunc(r, index)} onChange={(newValue) => field.OnChange(`${currentDateParts[0] ?? ""}-${newValue}-${currentDateParts[2] ?? ""}`)} classes={["edit-date-month"]} minLength={2} onKeyDown={(k) => onKeyDown(index, k)} />;
+						return <Input key={id} id={id} part={part} disabled={disabled} currentValue={current} width="2em" minLength={2} setRefFunc={r => addRefFunc(r, index)} onChange={newValue => { gotoNextPartWhenReady(newValue, part.type, index); field.OnChange(`${current["year"]}-${newValue}-${current["day"]}${withTimeParts ? `T${current["hour"]}:${current["minute"]}:${current["second"]}` : ""}`); }} onKeyDown={k => onKeyDown(index, k)} />;
 					case "year":
-						return <Input key={field.FieldId+"-"+part.type} id={field.FieldId+"-"+part.type} currentValue={currentDateParts[0] ?? ""} part={part.type} width="3em" disabled={disabled} setRefFunc={(r) => addRefFunc(r, index)} onChange={(newValue) => field.OnChange(`${newValue}-${currentDateParts[1] ?? ""}-${currentDateParts[2] ?? ""}`)} classes={["edit-date-year"]} minLength={4} onKeyDown={(k) => onKeyDown(index, k)} />;
+						return <Input key={id} id={id} part={part} disabled={disabled} currentValue={current} width="3em" minLength={4} setRefFunc={r => addRefFunc(r, index)} onChange={newValue => { gotoNextPartWhenReady(newValue, part.type, index); field.OnChange(`${newValue}-${current["month"]}-${current["day"]}${withTimeParts ? `T${current["hour"]}:${current["minute"]}:${current["second"]}` : ""}`); }} onKeyDown={k => onKeyDown(index, k)} />;
 					case "literal":
 						return <Layout key={field.FieldId+"-"+index} direction="row" elementType="span" children={part.value} classes={["edit-date-literal"]} />;
 				}
@@ -64,20 +112,22 @@ export const DateField: React.FC<DateFieldProps> = ({ field, disabled, ...props 
 	);
 };
 
-const Input: React.FC<{ id: string; part: "literal"|"day"|"month"|"year"; currentValue: string; onChange: (newValue: string) => void; disabled?: boolean; classes: string[]; width: string; minLength: number; setRefFunc: (r: HTMLInputElement|null) => void; onKeyDown: (key: string, currentValue: string) => boolean }> = ({ id, part, currentValue, onChange, disabled, classes, width, minLength, setRefFunc, onKeyDown }) => {
+const Input: React.FC<{ id: string; part: Intl.DateTimeFormatPart; currentValue: Record<string, string>; onChange: (newValue: string) => void; disabled?: boolean; width: string; minLength: number; setRefFunc: (r: HTMLInputElement|null) => void; onKeyDown: (key: string, currentValue: string) => boolean }> = ({ id, part, currentValue, onChange, disabled, width, minLength, setRefFunc, onKeyDown }) => {
+	const current = currentValue[part.type];
 	const optionallyPrependZeroes = React.useCallback(() => {
-		if (currentValue.length < minLength && currentValue.length > 0) {
-			onChange(new Array<string>().addCount(minLength - currentValue.length, () => "0").join("") + currentValue);
+		if (current.length < minLength && current.length > 0) {
+			onChange(new Array<string>().addCount(minLength - current.length, () => "0").join("") + current);
 		}
-	}, [currentValue, minLength, onChange]);
+	}, [current, minLength, onChange]);
 
 	return (
 		<InputField
 			type="number"
-			id={id} classes={classes}
+			id={id}
+			classes={[`edit-date-${part.type}`]}
 			disabled={disabled}
-			value={currentValue}
-			placeholder={{ Key: `LabelDate${part}` }}
+			value={current}
+			placeholder={{ Key: `LabelDate-${part.type}` }}
 			onChange={(v) => onChange(v ?? "")}
 			onKeyDown={onKeyDown}
 			ref={(r) => setRefFunc(r)}
